cfd49da4f761e08e2363a6ae30b7547ae7c725ad
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-03-17 12:45:59 +0530

    Create p79.c

---
+++ p79.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a[1000],n,i,j,t=0,k=0,m=0,b[1000],max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            t=a[i]-a[j];
+            b[k]=t;
+            k++;
+            m++;
+        }
+    }
+    max=b[0];
+    for(k=1;k<m;k++)
+    {
+        if(max<b[k])
+        {
+            max=b[k];
+        }
+    }
+    printf("%d",max);
+}
